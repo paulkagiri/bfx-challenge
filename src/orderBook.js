1088c16e8ccf1ee5afbec76ca2025ccfeeae825b
Paul Kagiri <paulkagirim@gmail.com>
2023-10-21 10:33:17 +0300

    Added ability to fulfill orders

--- src/orderBook.js
+++ src/orderBook.js
@@ -59,6 +59,100 @@ class OrderBook {
     debug("Buy orders", this.buys);
     debug("Sell orders", this.sells);
   }
+
+  // Method to fulfill an order
+  fulfillOrder(order) {
+    const matchedOrders = [];
+    let remainingAmountToMatch = order.amount;
+
+    if (remainingAmountToMatch > 0) {
+      // Buy order fulfillment
+      debug(`Buy lookup for ${remainingAmountToMatch} at ${order.price}`);
+      debug("First Selling order:", this.sells[0]);
+
+      while (remainingAmountToMatch > 0 && this.sells.length > 0 && order.price >= this.sells[0].price) {
+        const matchedOrder = this.sells.shift();
+        debug("Matching order:", matchedOrder);
+
+        if (remainingAmountToMatch === -matchedOrder.amount) {
+          // Exact match
+          matchedOrders.push(matchedOrder);
+          remainingAmountToMatch = 0;
+        } else if (remainingAmountToMatch < -matchedOrder.amount) {
+          // Partial match, reduce the remaining part of the sell order
+          matchedOrder.amount += remainingAmountToMatch;
+          this.sells.unshift(matchedOrder);
+          remainingAmountToMatch = 0;
+        } else {
+          // Partial match, deduct the matched order's amount
+          remainingAmountToMatch += matchedOrder.amount;
+          matchedOrders.push(matchedOrder);
+        }
+        debug("Amount remaining to match", remainingAmountToMatch);
+      }
+
+      if (remainingAmountToMatch === 0) {
+        matchedOrders.push(order);
+      }
+    } else {
+      // Sell order fulfillment
+      debug(`Sell lookup for ${remainingAmountToMatch} at ${order.price}`);
+      debug("First Buying order:", this.buys[0]);
+
+      while (remainingAmountToMatch < 0 && this.buys.length > 0 && order.price <= this.buys[0].price) {
+        const matchedOrder = this.buys.shift();
+        debug("Matching order:", matchedOrder);
+
+        if (remainingAmountToMatch === -matchedOrder.amount) {
+          // Exact match
+          matchedOrders.push(matchedOrder);
+          remainingAmountToMatch = 0;
+        } else if (remainingAmountToMatch > -matchedOrder.amount) {
+          // Partial match, reduce the remaining part of the buy order
+          matchedOrder.amount += remainingAmountToMatch;
+          this.buys.unshift(matchedOrder);
+          remainingAmountToMatch = 0;
+        } else {
+          // Partial match, deduct the matched order's amount
+          remainingAmountToMatch += matchedOrder.amount;
+          matchedOrders.push(matchedOrder);
+        }
+        debug("Amount remaining to match", remainingAmountToMatch);
+      }
+
+      if (remainingAmountToMatch === 0) {
+        matchedOrders.push(order);
+      }
+    }
+
+    // Return the matched orders and the remaining amount to match
+    return { matchedOrders, remainingAmountToMatch };
+  }
+
+  // Method to place a market order
+  placeMarketOrder(order) {
+    const { matchedOrders, remainingAmountToMatch } = this.fulfillOrder(order);
+    debug("Matched orders:", matchedOrders);
+
+    if (remainingAmountToMatch !== 0) {
+      // Place the remaining part of the order in the order book
+      order.amount = remainingAmountToMatch;
+      this.addOrder(order);
+    }
+
+    // Return true if any orders were matched, otherwise false
+    return matchedOrders.length > 0;
+  }
+
+  // Method to get the total number of orders in the order book
+  getOrderBookLength() {
+    return this.buys.length + this.sells.length;
+  }
+
+  // Method to retrieve all orders in the order book
+  getAllOrders() {
+    return [...this.buys, ...this.sells];
+  }
 }
 
 // Export the OrderBook class for use in other modules
